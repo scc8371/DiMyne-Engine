f316a9c203806608dad58bd928c5e87d03bbb59a
Sami Chamberlain <70332182+scc8371@users.noreply.github.com>
2023-09-08 22:07:47 -0400

    demo created for myne

--- src/audio/Sound.cpp
+++
@@ -1,9 +0,0 @@
-#include "Sound.h"
-
-Sound::Sound(const char* path){
-    auto sound = Wav(path);
-
-    alGenBuffers(1, (ALuint*)(&buffer));
-
-    alBufferData(buffer, AL_FORMAT_STEREO16, sound.getData(), sound.getSize(), sound.getFreq());
-}
